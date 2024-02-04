4a78a60c42ccf5f98c548f3ceaf9420c3ad9f5d5
thunv <thuitptit@gmail.com>
2024-02-04 22:24:17 +0700

    add sample with middleware

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -2,4 +2,9 @@
 
 var builder = WebApplication.CreateWebBuilder();
 var app = builder.Build();
+app.Use((next) => (context) =>
+{
+    Console.WriteLine("First middleware");
+    return next(context);
+});
 app.Run();  
